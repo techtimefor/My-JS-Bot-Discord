57d2bd2e809cd7482203ffd206128f38aeffda81
techtimefor <51377400+techtimefor@users.noreply.github.com>
2019-06-14 20:08:48 +0100

    Followed an Online Tourial for Code
    Do whatever you want with the code

---
+++ bot.js
@@ -0,0 +1,101 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+bot_secret_token = "TOKEN GOES HERE"
+
+console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+    })
+
+  var generalChannel = client.channels.get("CHANNEL_ID") 
+    generalChannel.send("Whats up!")  
+})
+
+client.on('message', (receivedMessage) => {
+    >> stop bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+    receivedMessage.channel.send("Message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
+})
+
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+    
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+})
+
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "multiply") {
+        multiplyCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+    }
+}
+
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
+
+function multiplyCommand(arguments, receivedMessage) {
+    if (arguments.length < 2) {
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
+        return
+    }
+    let product = 1 
+    arguments.forEach((value) => {
+        product = product * parseFloat(value)
+    })
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+}
+
+client.on('ready', () => {
+    >>bot to play game
+    client.user.setActivity("Chilling")
+    })
+
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+
+    // You can copy/paste the actual unicode emoji in the code (not _every_ unicode emoji works)
+    receivedMessage.react("👍")
+    receivedMessage.react("🛐")
+    >>emojis: https://unicode.org/emoji/charts/full-emoji-list.html
+
+    // Get every custom emoji from the server (if any) and react with each one
+    receivedMessage.guild.emojis.forEach(customEmoji => {
+        console.log(`Reacting with custom emoji: ${customEmoji.name} (${customEmoji.id})`)
+        receivedMessage.react(customEmoji)
+    })
+    // If you know the ID of the custom emoji you want, you can get it directly with:
+    // let customEmoji = receivedMessage.guild.emojis.get(emojiId)
+})    
+
+client.login(bot_secret_token)
+
